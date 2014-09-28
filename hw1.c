decb01b25ad89fad9195fcc8cecfdf3ab8349dd8
Jian-Long Huang <jianlong@ntu.edu.tw>
2014-09-29 01:41:47 +0800

    hw1: get input from the standard input

--- hw1.c
+++ hw1.c
@@ -2,21 +2,23 @@
 
 int main()
 {
-        int a = 117;
-        int b = 203;
-        int c = 151;
-        int d = 29;
-        int e = 37;
+        int a, b, c, d, e;
+        scanf("%d", &a);
+        scanf("%d", &b);
+        scanf("%d", &c);
+        scanf("%d", &d);
+        scanf("%d", &e);
 
         int vol_total = a * b * c;
         int ap = a - 2 * e;
         int bp = b - 2 * e;
         int cp = c - 2 * e;
         int vol = vol_total - d * 2 * (ap * bp + ap * cp + bp * cp);
-        printf("Volume: %d\n", vol);
-
         int sa = 2 * (a * b + a * c + b * c) + 8 * d * (ap + bp + cp);
-        printf("Surface area: %d\n", sa);
+
+        printf("\n");
+        printf("Surface area, Volume:\n");
+        printf("%d %d\n", sa, vol);
 
         return 0;
 }
